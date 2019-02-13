804aa87d8e892820e09142ddd3e9b55bd85097a6
rotonmeta <roton-meta@live.com>
2019-02-13 10:20:56 -0500

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/SpreadsheetQtWidget.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpreadsheetQtWidget.dir/main.cpp.o"
+  "SpreadsheetQtWidget.pdb"
+  "SpreadsheetQtWidget"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SpreadsheetQtWidget.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SpreadsheetQtWidget.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SpreadsheetQtWidget.
+# This may be replaced when dependencies are built.
