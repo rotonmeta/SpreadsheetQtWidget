24e590fe9eb118aa6240aa51cce735e42fe72835
rotonmeta <roton-meta@live.com>
2019-02-13 11:17:09 -0500

    added observer and subject

---
+++ Observer.h
@@ -0,0 +1,13 @@
+//
+// Created by osboxes on 2/13/19.
+//
+
+#ifndef SPREADSHEETQTWIDGET_OBSERVER_H
+#define SPREADSHEETQTWIDGET_OBSERVER_H
+
+class Observer {
+public:
+    virtual void update() = 0;
+    virtual ~Observer() {};
+};
+#endif //SPREADSHEETQTWIDGET_OBSERVER_H
---
+++ Subject.h
@@ -0,0 +1,19 @@
+//
+// Created by osboxes on 2/13/19.
+//
+
+#ifndef SPREADSHEETQTWIDGET_SUBJECT_H
+#define SPREADSHEETQTWIDGET_SUBJECT_H
+
+#include "Observer.h"
+
+class Subject {
+public:
+    virtual void subscribe(Observer *observer) = 0;
+    virtual void unsubscribe(Observer *observer) = 0;
+    virtual void notify() = 0;
+
+    virtual ~Subject() {};
+
+};
+#endif //SPREADSHEETQTWIDGET_SUBJECT_H
